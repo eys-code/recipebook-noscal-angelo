bcf4a4de0221b2da97341bd4f2815518c01da6f6
eys-coder <angelo.noscal@student.ateneo.edu>
2025-04-08 16:22:52 +0800

    Add migrations for lab3

--- recipebook-noscal-angelo/ledger/models.py
+++ recipebook-noscal-angelo/ledger/models.py
@@ -1,36 +1,49 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 from django.db import models
 from django.urls import reverse
 
-class Recipe(models.Model):
-    name = models.CharField(max_length=100)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(
+        validators=[
+            MinLengthValidator(255, "Bio must be at least 255 characters long.")
+        ]
+    )
    
     def __str__(self):
         return self.name
-   
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="recipe", null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
     def get_absolute_url(self):
-        return reverse('ledger:recipe_detail', args=str(self.pk))
-    
-    class Meta:
-        verbose_name = 'recipe'
+        return reverse("ledger:recipe_detail", args=str(self.pk))
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
-   
+    name = models.CharField(max_length=100, unique=True)
+
     def __str__(self):
         return self.name
 
     def get_absoulute_url(self):
-        return reverse('ledger:ingredient_detail', args=str(self.pk))
-    
-    class Meta:
-        verbose_name = 'ingredient'
-
+        return reverse("ledger:ingredient_detail", args=str(self.pk))
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
-   
-    def __str__(self):
-        return f'{self.quantity} {self.ingredient.name}'
+    ingredient = models.ForeignKey(
+        Ingredient, on_delete=models.CASCADE, related_name="recipe"
+    )
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name="ingredients"
+    )
 
+    def __str__(self):
+        return f"{self.quantity} {self.ingredient.name} in {self.recipe.name}"
